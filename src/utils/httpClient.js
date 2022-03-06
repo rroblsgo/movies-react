1dfa700afc53617f8be7e928025b86ba17240de5
Ricardo Robles <rroblesgo@gmail.com>
2022-03-06 20:05:21 +0100

    Ajuste variables de entorno

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,9 +1,10 @@
-const API = 'https://api.themoviedb.org/3';
+const API = process.env.REACT_APP_API;
+const API_TOKEN = process.env.REACT_APP_API_TOKEN;
 
 export function get(path) {
   return fetch(API + path, {
     headers: {
-      Authorization: process.env.REACT_APP_TOKEN,
+      Authorization: 'Bearer ' + API_TOKEN,
       'Content-Type': 'application/json;charset=utf-8',
     },
   }).then((result) => result.json());
